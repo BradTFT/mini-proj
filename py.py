5bfb7015919804aea745995cae4c2ebe919e9e0b
BradTFT <bradtft@outlook.com>
2022-04-25 13:05:57 -0400

    attempted to fix (unsucessful)

--- py.py
+++ py.py
@@ -1,13 +1,25 @@
 #! doesnt work
 import random
 
-number = input('Enter your number. >>> ')
-
+number = int(input('Enter your number. >>> '))
 guess = random.randrange(1, 10)
+correct = input(f'Was your number {guess}?')
+again = input('I guessed correctly. Wanna play again? >>> ')
+inagain = input('I guessed incorrectly. Wanna Play again? >>> ')
 
-while number != guess:
-    # reassign `guess`
-    guess = random.randrange(1, 10)
-    print(guess)
-    if number == guess:
-        print(f'Was your number {guess}')
+def game():
+    while number != guess:
+        # reassign `guess`
+        guess = random.randrange(1, 10)
+        if number == guess:
+            correct = input(f'Was your number {guess}?')
+        
+if correct == 'y' or correct == 'Y':
+    again = input('I guessed correctly. Wanna play again? >>> ')
+elif correct == 'n' or correct == 'N':
+    inagain = input('I guessed incorrectly. Wanna Play again? >>> ')
+
+if again == 'Y' or again == 'y' or inagain == 'Y' or inagain == 'y':
+    game()
+else:
+    pass
