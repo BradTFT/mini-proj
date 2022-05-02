4f00d018d6f6fed0b9ac3757b9d7333c8432496c
BradTFT <bradtft@outlook.com>
2022-05-02 12:07:46 -0400

    Fix error 2
    New erroradded. See README.md error 5

--- scraping/webscraper.py
+++ scraping/webscraper.py
@@ -6,19 +6,30 @@ import re
 
 gpu = input('product >>> ')
 
-url = 'https://www.newegg.com/p/pl?d=3080&N=8000%204131'
+url = f'https://www.newegg.com/p/pl?d={gpu}&N=4131'
 page = requests.get(url).text
 doc = BeautifulSoup(page, 'html.parser')
 
 page_text = doc.find(class_='list-tool-pagination-text').strong
 pages = int(str(page_text).split('/')[-2].split('>')[-1][:-1])
 
+#! i found a yt short regarding out of index range issues using for in range. im going to try this 
+#! and see if it fixes the issue with this script.
 
-for page in range(1, pages, +1):
-    url = f'https://www.newegg.com/p/pl?d={gpu}3080&N=8000%204131&page={page}'
+"""old:
+for page in range(1, pages, +1)"""
+#! works
+#new:
+list2 = 1, pages, +1
+
+for page in list2:
+    url = f'https://www.newegg.com/p/p1?d={gpu}&N=4131&page={page}'
     page = requests.get(url).text
     doc = BeautifulSoup(page, 'html.parser')
+    
+    #! new issue: README.md Error 5
+    div = doc.find(class_="item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell")
+    items = div.find_all(text=re.compile(gpu))
 
-    items = doc.find_all(text=re.compile(gpu))
     for item in items:
         print(item)
