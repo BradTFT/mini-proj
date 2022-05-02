0a4db865b3db0817be28ea2511486e81998c96e7
BradTFT <bradtft@outlook.com>
2022-05-02 11:20:26 -0400

    Update class.py

---
+++ py-files/class.py
@@ -0,0 +1,12 @@
+
+
+#im trying to figure out what im supposed to do with classes. i sort of understand what they are
+#and what they do but im  not sure how to use them in practical code and things like that.
+class Test:
+    def __init__(self) -> None:
+        self.name = []
+        self.age = []
+
+
+print(Test())
+
