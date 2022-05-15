eb2b99af4e3f4246205602a569817df2b874e3be
BradTFT <bradtft@outlook.com>
2022-05-15 14:37:05 -0400

    Add comment to ticgui.py

--- py-files/gui/ticgui.py
+++ py-files/gui/ticgui.py
@@ -1,20 +1,18 @@
 from tkinter import *
 import tkinter as tk
 #! basic click detection is all set. Just need to position everything to a grid.
+#! cant figure out how to change the button text onClick
 root = Tk()
 
 def lclick(event):
-    buttontest.set('X')
     print('this button is now X')
 
 
 def rclick(event):
-    buttontest.set('O')
     print('this button is now O')
 
 
-
-buttontest = Button(root, text='')
+buttontest = Button(root)
 
 buttontest.bind('<Button-1>', lclick)
 buttontest.bind('<Button-3>', rclick)
