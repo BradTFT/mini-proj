4b7a7626a73a600d022eab4345f79b08ad9047b1
BradTFT <bradtft@outlook.com>
2022-05-23 15:17:42 -0400

    Add comments and debugging tools

--- py/madlibs.py
+++ py/madlibs.py
@@ -1,44 +1,17 @@
-#! work in progress
-
 import random
-
-#!attempting to fix
-
-start = input('Press ENTER to start')
-
-#entries:
-'''
-first = input()
-second = input()
-third = input()
-fourth = input()
-fifth = input()
-sixth = input()
-seventh = input()
-'''
-#i want multiple different stories. so each will be its own function and the random module will chose one
-
-
-
-    
-
-
-#different games functions(these are the different varaitions of each game)
+#idea is that the functions are going to ask for input for each replacable word and then stitch it together and print the result
+#games
 def game1():
     pass
-
 
 def game2():
     pass
 
-
 def game3():
     pass
 
-
 def game4():
     pass
-
 
 def game5():
     pass
@@ -46,7 +19,21 @@ def game5():
 def game6():
     pass
 
-#!trying to chose a random function but its calling all of them instead
-games_list = [game1(), game2(), game3(), game4(), game5(), game6()]
 
-random.choice(games_list)
+#game chooser
+getGame = random.randrange(1, 6)
+#debugger
+print(getGame)
+
+if getGame == 1:
+    game1()
+elif getGame == 2:
+    game2()
+elif getGame == 3:
+    game3()
+elif getGame == 4:
+    game4()
+elif getGame == 5:
+    game5()
+elif getGame == 6:
+    game6()
